8da942b56b4c11e018e95c97362e0f54c22666ed
Monis9451 <mianmonishussain786@gmail.com>
2025-04-03 18:15:40 +0500

    make a change in vite.config.js file for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,74 +1,124 @@
-import React, {useState, useEffect} from 'react'
-import Search from './components/search'
+import React, { useState, useEffect } from 'react';
+import { useDebounce } from 'react-use';
+import Search from './components/Search';
+import Loader from './components/Loader';
+import MovieCard from './components/MovieCard';
+import {updateSearchCount} from './appwrite';
+import { getTrendingMovies } from './appwrite';
 
 const API_BASE_URL = 'https://api.themoviedb.org/3';
-
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 
 const API_OPTIONS = {
     method: 'GET',
-    headers:{
+    headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${API_KEY}`
-    }
-}
+        Authorization: `Bearer ${API_KEY}`,
+    },
+};
 
 const App = () => {
     const [searchValue, setSearchValue] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
     const [moviesList, setMoviesList] = useState([]);
     const [loading, setLoading] = useState(false);
-    const fetchMovies = async () => {
+    const [debounceSearch, setDebounceSearch] = useState('');
+    const [trendingMovies, setTrendingMovies] = useState([]);
+
+    useDebounce(
+        () => {
+            setDebounceSearch(searchValue);
+        },
+        600,
+        [searchValue]
+    );
+
+    const fetchMovies = async (query = '') => {
         setLoading(true);
         try {
-            const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+            const endpoint = query
+                ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+                : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
             const response = await fetch(endpoint, API_OPTIONS);
-            if(!response.ok) {
+            if (!response.ok) {
                 throw new Error('Failed to fetch movies');
             }
             const data = await response.json();
-            console.log(data);
-            if(data.Response === 'False') {
-                setErrorMessage(data.Error ||  'Failed to fetch movies');
+            if (data.Response === 'False') {
+                setErrorMessage(data.Error || 'Failed to fetch movies');
             }
             setMoviesList(data.results || []);
-        }
-        catch (error){
+            if(query && data.results.length > 0){
+                await updateSearchCount(query, data.results[0]);
+            }
+        } catch (error) {
             console.error('Error fetching movies:', error);
             setErrorMessage('Failed to fetch movies. Please try again later.');
-        }
-        finally {
+        } finally {
             setLoading(false);
         }
+    };
+
+    const fetchTrendingMovies = async () => {
+        try {
+            const movies = await getTrendingMovies();
+            setTrendingMovies(movies);
+        } catch (error) {
+            console.error('Error fetching trending movies:', error);
+        }
     }
+
     useEffect(() => {
-        fetchMovies();
-    }, [])
-  return (
-    <main>
-        <div className='pattern'/>
-            <div className='wrapper'>
+        fetchMovies(debounceSearch);
+    }, [debounceSearch]);
+
+    useEffect(() => {
+        fetchTrendingMovies();
+    },[])
+
+    return (
+        <main>
+            <div className="pattern" />
+            <div className="wrapper">
                 <header>
                     <img src="../public/hero-img.png" alt="Hero Banner" />
-                    <h1>Find <span className='text-gradient'>Movies</span> You'll Love Without the Hassle</h1>
-                    <Search searchValue={searchValue} setSearchValue={setSearchValue}/>
+                    <h1>
+                        Find <span className="text-gradient">Movies</span> You'll Love Without the Hassle
+                    </h1>
+                    <Search searchValue={searchValue} setSearchValue={setSearchValue} />
                 </header>
-                <section className='all-movies'>
-                    <h1>All Movies</h1>
-                    {loading?
-                     <p className='text-white'>Loading...</p> : 
-                     setErrorMessage? <p className='text-red-500'>{errorMessage}</p> :
-                     (
+                {trendingMovies.length > 0 && (
+                    <section className="trending">
+                        <h2>Trending Movies</h2>
                         <ul>
-                            {moviesList.map((movie) => (
-                                <p className='text-white'>{movie.original_title}</p>
+                            {trendingMovies.map((movie, index) => (
+                                <li key={movie.$id}>
+                                    <p>{index + 1}</p>
+                                    <img src={movie.poster_url} alt={movie.searchValue} />
+                                </li>
                             ))}
                         </ul>
-                     )}
+                    </section>
+                )}
+                <section className="all-movies">
+                    <h2>All Movies</h2>
+                    {loading ? (
+                        <p className="text-white">
+                            <Loader />
+                        </p>
+                    ) : errorMessage ? (
+                        <p className="text-red-500">{errorMessage}</p>
+                    ) : (
+                        <ul>
+                            {moviesList.map((movie) => (
+                                <MovieCard key={movie.id} movie={movie} />
+                            ))}
+                        </ul>
+                    )}
                 </section>
             </div>
-    </main>
-  )
-}
+        </main>
+    );
+};
 
-export default App
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
-import dotenv from 'dotenv';
-
-dotenv.config();
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
-});
+  base: import.meta.env.VITE_BASE_PATH || '/React-Movie-App',
+})
